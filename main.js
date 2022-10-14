8211a364c7d9ab370a7c85b64a99d2687dac504c
Naitik Karn <84630339+Naitik2222@users.noreply.github.com>
2022-10-14 19:39:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
+video="";
+Status="";
+
 function setup(){
     canvas=createCanvas(380,380);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.sizeO(380,380);
+    video.hide();
+
+}
+
+function start(){
+    objectDetector=ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting Objects";
+    object_name = document.getElementById("input_box").value;
+}
+function modelLoaded(){
+    console.log("Model Loaded");
+    Status = true;
     
+}
+
+function draw(){
+  image(video , 0 ,0 , 380 , 380);
 }
